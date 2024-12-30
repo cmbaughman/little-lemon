c3077ede891cd13fded0fd91b371cef965c7daa0
Chris Baughman <cmbaughman@outlook.com>
2024-12-30 16:24:49 -0500

    Added main section components (header, nav, main, footer)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <>
+      <Header />
+      <Navigation />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
